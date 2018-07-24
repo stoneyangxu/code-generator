a60c81012454b185568d1550435e25157c8801ec
stoneyangxu <stoneyangxu@icloud.com>
2018-07-25 01:08:57 +0800

    make targetPath to be current if ignore the 3rd parameter

--- src/__test__/dispatch.spec.js
+++ src/__test__/dispatch.spec.js
@@ -18,12 +18,19 @@ describe('dispatch', () => {
   it('should return error when there is not enough parameters', () => {
     dispatch().should.equal(0);
     dispatch([0]).should.equal(0);
-    dispatch([0, 1]).should.equal(0);
+    dispatch([0, 1]).should.equal(1);
     dispatch([0, 1, 2]).should.equal(1);
   });
 
   it('should call generator when action is g', () => {
     dispatch(['g', 'editorconfig', './']);
+    generator.generator.calledOnce.should.equal(true);
+    generator.generator.getCall(0).args[0].should.equal('editorconfig');
+    generator.generator.getCall(0).args[1].should.equal('./');
+  });
+
+  it('should make targetPath to be current if ignore the 3rd parameter', () => {
+    dispatch(['g', 'editorconfig']);
     generator.generator.calledOnce.should.equal(true);
     generator.generator.getCall(0).args[0].should.equal('editorconfig');
     generator.generator.getCall(0).args[1].should.equal('./');
--- src/dispatch.js
+++ src/dispatch.js
@@ -2,10 +2,11 @@ import { step, error } from './utils/console';
 import { generator } from './generator';
 
 export default function dispatch(argv, program) {
-  if (!argv || argv.length < 3) {
+  if (!argv || argv.length < 2) {
     return 0;
   }
-  const [action, cmd, targetPath] = argv;
+
+  const [action, cmd, targetPath] = argv.length > 2 ? argv : [...argv, './'];
   switch (action) {
     case 'g':
       step(`start generating ${cmd}`);
